6101be6eedd50c9c4dfbd0faf2f8612bf1a0f216
Chung-Chi-Lin <z0925955648@gmail.com>
2022-11-18 00:03:06 +0800

    first update

---
+++ javascripts/all.js
@@ -0,0 +1,8 @@
+// all.html- sidebar
+const toggleMenuBtn =document.querySelector('#toggle-btn');
+const body =document.querySelector('body');
+
+toggleMenuBtn.addEventListener('click',(e) => {
+    e.preventDefault();
+    body.classList.toggle('sidebar-toggled');
+})
